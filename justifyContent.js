cba52a0c2a477348fbb4346d81bc782590ccf2d9
Suzana <su_zana-santos@hotmail.com>
2022-06-20 21:06:35 -0300

    adicionando novos tipos de flex-box

---
+++ justifyContent.js
@@ -0,0 +1,11 @@
+/*se encarrega de alinhar os itens dentro do container
+de acordo com a direção pretendida e trata da distribuição de espaçamento entre eles */
+
+/*
+flex-start: inicio do container.
+flex-end: final do container.
+center: ao centro do container.
+space-between: cria um espaçamento igual entre os elementos.
+space-around: os espacamentos do meiosão duas vezes maiores que o inicial e final.
+*/
+
